3454065f6bcfb5fd4235cf17300b4633431bc013
Tania <tania.asc@gmail.com>
2017-07-25 22:00:15 +0200

    Loops introduced

--- chat.py
+++ chat.py
@@ -11,6 +11,7 @@ def read_message():
     while income_string.isspace() or income_string == '':
         sys.stdout.write("Your message is empty. Please try again. ")
         income_string = input()
+#    trim
     income_words = np.unique(np.array(income_string.split(' ')))
     return income_words  
 
@@ -19,8 +20,18 @@ def ask_first_question():
     sys.stdout.write('Hello! What kind of device are you interested in?\n') 
     sys.stdout.write('Tip: you can type "abort" any time to abort the session. ')
                
-def ask_to_choose(column_uniques):
+def ask_to_choose(column_uniques, column_name):
     '''Asks to enter a number from numbers to choose'''
+    if column_name == 'Category':
+        sys.stdout.write('We found products correponding to your query in '
+          +str(column_uniques.shape[0])+ ' categories:\n')
+    if column_name == 'Brand':
+        sys.stdout.write('We found products correponding to your query from '
+          +str(column_uniques.shape[0])+ ' brands:\n')
+    if column_name == 'Product Name':
+        sys.stdout.write('We found '+str(column_uniques.shape[0])+ 
+                                         ' products correponding to your query:\n')
+    
     sys.stdout.write("\n".join([str(i)+': ' + column_uniques[i] 
                                 for i in range(column_uniques.shape[0])]))
     sys.stdout.write('\nPlease enter the corresponding number: ')
@@ -54,7 +65,7 @@ def reset():
     '''Runs a new session'''
     sys.stdout.write("You aborted the session. Let's try again.\n")
     new_main()
-      
+         
 def new_query(data_reduction, words, column_name, remaining_words=1):
     #initialize remaining_words matrix if the beginning of conversation
     if type(remaining_words) ==int:
@@ -73,78 +84,50 @@ def new_query(data_reduction, words, column_name, remaining_words=1):
     remaining_words = remaining_words[candidate_rows,:]
     return data_reduction, remaining_words
     
+def choosing(data_reduction, column_name):
+    '''Asks to choose from unique values in column_name. Narrows data according to the answer'''
+    column_uniques = data_reduction[column_name].unique()
+    ask_to_choose(column_uniques, column_name)
+    income_words = read_message()
+    #check for correct input
+    while not check_index(income_words, column_uniques):
+        reset_var = check_reset(income_words)
+        if reset_var:            
+            return reset_var, data_reduction 
+        ask_to_choose_again()
+        income_words = read_message()
+    index = int(income_words[0])
+    data_reduction=data_reduction.loc[data_reduction[column_name]==column_uniques[index]]
+    return False, data_reduction
+    
    
     
 def new_main():
     data_reduction = data
+    remaining_words=1
     ask_first_question()
     income_words = read_message()
     #check for session reset
     if check_reset(income_words):
         reset()
         return
-    #check for words in Tags, only most relevant rows remain
-    data_reduction, remaining_words = new_query(data_reduction,income_words, 'Tags')
-    #if the decision is not made: afterwards check for words in Product names, only most relevant rows remain
-    if data_reduction.shape[0]>1:
-        data_reduction, remaining_words = new_query(data_reduction,income_words, 'Product Name', remaining_words)
-        #if the decision is not made: afterwards check for words in Brands, only most relevant rows remain
+    #check for words in Tags, afterwards Product Name, afterwards Brand; each time only most relevant rows remain
+    for column_name in ['Tags', 'Product Name', 'Brand']:
         if data_reduction.shape[0]>1:
-            data_reduction, remaining_words = new_query(data_reduction,income_words, 'Brand', remaining_words)
+            data_reduction, remaining_words = new_query(data_reduction,income_words, column_name, remaining_words)
     #If words were not found, start from the beginnig
     if data_reduction.shape[0] == data.shape[0]:
         suggest_random(data_reduction)
         return
     #if the decision is not made, ask a customer to choose
     if data_reduction.shape[0] != 1:
-        #loop?
         #first a customer chooses a category (if it's ambiguous)
-        if data_reduction['Category'].unique().shape[0]>1:
-            categories = data_reduction['Category'].unique()
-            sys.stdout.write('We found products correponding to your query in '
-                  +str(data_reduction['Category'].unique().shape[0])+ ' categories\n')
-            ask_to_choose(categories)
-            income_words = read_message()
-            #check for correst_input
-            while not check_index(income_words, categories):
-                if check_reset(income_words):
-                    reset()
-                    return
-                ask_to_choose_again()
-                income_words = read_message()
-            index = int(income_words[0])
-            data_reduction=data_reduction.loc[data_reduction['Category']==categories[index]]
-        #second a customer chooses a brand (if it's ambiguous)        
-        if data_reduction['Brand'].unique().shape[0]>1:
-            brands = data_reduction['Brand'].unique()
-            sys.stdout.write('We found products correponding to your query from '
-                  +str(data_reduction['Brand'].unique().shape[0])+ ' brands\n')
-            ask_to_choose(brands)
-            income_words = read_message()
-            #check for correst_input
-            while not check_index(income_words, brands):
-                if check_reset(income_words):
-                    reset()
-                    return
-                ask_to_choose_again()
-                income_words = read_message()
-            index = int(income_words[0])
-            data_reduction=data_reduction.loc[data_reduction['Brand']==brands[index]]
-        #finally a customer chooses a product (if it's ambiguous)    
-        if data_reduction['Product Name'].unique().shape[0]>1:
-            names = data_reduction['Product Name'].unique()
-            sys.stdout.write('We found '+str(names.shape[0])+' products correponding to your query\n')
-            ask_to_choose(names)
-            income_words = read_message()
-            #check for correst_input
-            while not check_index(income_words, names):
-                if check_reset(income_words):
-                    reset()
-                    return
-                ask_to_choose_again()
-                income_words = read_message()
-            index = int(income_words[0])
-            data_reduction=data_reduction.loc[data_reduction['Product Name']==names[index]]
+        for column_name in ['Category', 'Brand','Product Name']:
+            if data_reduction[column_name].unique().shape[0]>1:
+               reset_var, data_reduction = choosing(data_reduction, column_name)
+               if reset_var:
+                   reset()
+                   return
     #When only 1 item left, show a customer it's subscription price
     suggest(data_reduction)
 
